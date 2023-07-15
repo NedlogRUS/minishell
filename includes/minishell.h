0c42511b8fc24cd0d44d48394b8f044fa735d494
apanikov <panikov.a.a@gmail.com>
2023-07-15 20:54:30 +0400

    pwd test

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: apanikov <apanikov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 19:13:02 by apanikov          #+#    #+#             */
-/*   Updated: 2023/07/14 15:33:56 by apanikov         ###   ########.fr       */
+/*   Updated: 2023/07/15 20:17:45 by apanikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <limits.h>
 
 typedef struct s_mhstuct
 {
--- src/main/main.c
+++ src/main/main.c
@@ -6,11 +6,31 @@
 /*   By: apanikov <apanikov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 19:33:46 by apanikov          #+#    #+#             */
-/*   Updated: 2023/07/14 16:01:10 by apanikov         ###   ########.fr       */
+/*   Updated: 2023/07/15 20:17:31 by apanikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+int pwd(t_mhstruct *mh) 
+{
+    char cwd[PATH_MAX];
+
+	if (!ft_strcmp("pwd", mh->input))
+	{
+    	if (getcwd(cwd, sizeof(cwd)) != NULL) 
+		{
+    	    printf("%s\n", cwd);
+    	} 
+		else 
+        	return 1;
+	}
+	else
+		return 1;
+    return 0;
+}
+
+//void start_builtin()
 
 int main(int ac, char **av, char **env)
 {
@@ -23,7 +43,8 @@ int main(int ac, char **av, char **env)
 	while(1)
 	{
 		mh->input = readline("$> ");
-		printf("%s\n", mh->input);
+		if(pwd(mh) != 0)
+			printf("minihell: command not found: %s\n", mh->input);
 		add_history(mh->input);
 	}
 	return 0;
